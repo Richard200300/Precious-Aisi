19c6d8d9bf3054460e894750c4d76defda994e4d
R1chard <103750454+Richard200300@users.noreply.github.com>
2023-08-15 17:28:57 +0100

    Update Payment.jsx

--- Frontend/src/checkout/components/Payment.jsx
+++ Frontend/src/checkout/components/Payment.jsx
@@ -3,8 +3,19 @@ import React from 'react'
 const Payment = () => {
    
   return (
-    <div>
-       
+    <div className='uppercase'>
+  <div className="flex">
+    <input type="radio" />
+    <label>apple pay </label>
+  </div>
+  <div className="flex">
+    <input type="radio" />
+    <p className="">apple  pay</p>
+  </div>
+  <div className="flex">
+    <input type="radio" />
+    <p className="">apple  pay</p>
+  </div>
 
 
     </div>
